e81815d502539d43f6b28759e1a731e91162d71f
RurouniKenshin24 <68823624+RurouniKenshin24@users.noreply.github.com>
2021-04-09 10:11:01 -0500

    Delete selenium-testng-cucumber-project directory

--- selenium-testng-cucumber-project/src/test/java/com/cybertek/library/utilities/common/LibraryConstants.java
+++
@@ -1,9 +0,0 @@
-package com.cybertek.library.utilities.common;
-
-public interface LibraryConstants {
-    String STUDENT = "student";
-    String LIBRARIAN = "librarian";
-    String DASHBOARD = "dashboard";
-    String USERS = "users";
-    String BOOKS = "books";
-}
